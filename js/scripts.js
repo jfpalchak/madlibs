9f53063c0b231743ad0f62c1605b511e98b664c2
Joey Palchak <joey.palchak@gmail.com>
2023-08-09 10:23:58 -0700

    First commit.
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+    const person1Input = document.getElementById("person1Input").value;
+    const person2Input = document.getElementById("person2Input").value;
+    const animalInput= document.getElementById("animalInput").value;
+    const exclamationInput = document.getElementById("exclamationInput").value;
+    const verbInput = document.getElementById("verbInput").value;
+    // Here's the first log we add.
+    
+    const nounInput = document.getElementById("nounInput").value;
+
+    document.querySelector("span#person1a").innerText = person1Input;
+    document.querySelector("span#person1b").innerText = person1Input;
+    document.querySelector("span#person1c").innerText = person1Input;
+    document.querySelector("span#person2a").innerText = person2Input;
+    document.querySelector("span#person2b").innerText = person2Input;
+    document.querySelector("span#animal").innerText = animalInput;
+    document.querySelector("span#verb").innerText = verbInput;
+    document.querySelector("span#noun").innerText = nounInput;
+    document.querySelector("span#exclamation").innerText = exclamationInput;
+
+    document.querySelector("div#story").removeAttribute("class");
+  };
+};
