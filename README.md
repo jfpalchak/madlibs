b926b45d11eaa257aea4fcb48af3fe77b944e53a
Joey Palchak <joey.palchak@gmail.com>
2023-08-09 19:51:11 -0700

    Add simple Bootstrap styling, as well as a Reset button.
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>

---
+++ README.md
@@ -0,0 +1,37 @@
+# MADLIBS
+
+#### By _**Joey Palchak & Paul LeTourneau**_
+
+#### Demonstration site showcasing understanding of Event Handling in JavaScript.
+
+## Technologies Used
+
+* HTML
+* CSS
+* Bootstrap
+* JavaScript
+
+## Description
+
+This is a simple demonstration site with minimal styling, showcasing an understanding of Event Handling in JavaScript.
+
+The user is presented with multiple forms to fill out, Madlib style, at which point the user can click a Submit button. Once clicked, text will appear in the lower body of the page, presenting the user with a story using words and phrases previously entered in the aforementioned forms. 
+
+When the Submit button is clicked and the story is presented, a Reset button is then presented to the user. If clicked, the story will disappear and the previous input values for each form will be reset to a null state. 
+
+The user can then repeat the process.
+
+## Setup/Installation Requirements
+
+* Clone the current repository to working directory.
+* Open index.html in your browser.
+
+## Known Bugs
+
+* User can input anything into form boxes and the page will print the respective input, without checking if the input is valid.
+
+## License
+
+MIT License
+
+Copyright (c) 8/09/2023 Joey Palchak & Paul LeTourneau
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
+// let's add a new function that resets all input values, 
+// and hides the story again!
+function resetStory() {
+  document.querySelector("div#story").setAttribute("class", "hidden")
+  document.getElementById("person1Input").value = null;
+  document.getElementById("person2Input").value = null;
+  document.getElementById("animalInput").value = null;
+  document.getElementById("exclamationInput").value = null;
+  document.getElementById("verbInput").value = null;
+  document.getElementById("nounInput").value = null;
+  document.querySelector("button#reset").setAttribute("class", "hidden");
+}
+
 window.onload = function() {
   let form = document.querySelector("form");
-  form.onsubmit = function(event) {
+  let resetButton = document.querySelector("button#reset")
+  
+  form.addEventListener("submit", function(event) {
     event.preventDefault();
     const person1Input = document.getElementById("person1Input").value;
     const person2Input = document.getElementById("person2Input").value;
@@ -22,5 +37,15 @@ window.onload = function() {
     document.querySelector("span#exclamation").innerText = exclamationInput;
 
     document.querySelector("div#story").removeAttribute("class");
-  };
+  });
+
+  // new event listener for submit event to show the reset button
+  form.addEventListener("submit", function() {
+    resetButton.removeAttribute("class");
+  });
+
+  // new event listener for click event on reset button to 
+  // reset form values and hide story
+  resetButton.addEventListener("click", resetStory);
+
 };
